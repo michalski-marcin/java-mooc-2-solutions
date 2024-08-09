946a8681e25f714f76999c8e60f74919ee9efd86
Marcin Michalski <mkamichalski@gmail.com>
2024-08-09 20:51:58 +0200

    IOweYou

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -17,14 +17,14 @@ public class Program {
         printValuesOfKeysWhere(hashmap, "e");
     }
 
-    public static void printKeys(HashMap<String,String> hashmap) {
+    public static void printKeys(HashMap<String, String> hashmap) {
         for (String key : hashmap.keySet()) {
             System.out.println(key);
-            
+
         }
     }
 
-    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
         for (String key : hashmap.keySet()) {
             if (key.contains(text)) {
                 System.out.println(key);
@@ -32,10 +32,10 @@ public class Program {
         }
     }
 
-    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
-        for (String value : hashmap.values()) {
-            if (value.contains(text)) {
-                System.out.println(value);
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
             }
         }
     }
