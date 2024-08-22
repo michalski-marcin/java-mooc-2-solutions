30f05b894ef0e886ddbc11647f7cd0f6da399299
Marcin Michalski <106470316+michalski-marcin@users.noreply.github.com>
2024-08-22 11:16:23 +0000

    Animals 22.08

---
+++ part09-Part09_12.Animals/src/main/java/Cat.java
@@ -0,0 +1,24 @@
+public class Cat extends Animal implements NoiseCapable {
+
+
+
+  public Cat() {
+    super("Cat");
+
+  }
+
+  public Cat(String name) {
+    super(name);
+
+  }
+
+  public void purr() {
+    System.out.println(this.getName() + " purrs");
+  }
+
+  public void makeNoise() {
+    this.purr()
+;  }
+
+
+}
---
+++ part09-Part09_12.Animals/src/main/java/Dog.java
@@ -0,0 +1,26 @@
+
+
+public class Dog extends Animal implements NoiseCapable  {
+
+  
+
+  public Dog() {
+    super("Dog");
+    
+  }
+
+  public Dog(String name) {
+    super(name);
+    
+  }
+
+  public void bark() {
+    System.out.println(this.getName() + " barks");
+  }
+
+  public void makeNoise() {
+    this.bark();
+  }
+
+  
+}
