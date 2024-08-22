1f89b746a563541e63169f034a43519767f44b13
Marcin Michalski <mkamichalski@gmail.com>
2024-08-22 14:34:04 +0200

    Animals 22.08

--- part09-Part09_12.Animals/src/main/java/Animal.java
+++ part09-Part09_12.Animals/src/main/java/Animal.java
@@ -1,21 +1,20 @@
 
-
-public class Animal {
+public abstract class Animal {
 
   private String name;
 
-  public Animal (String name) {
+  public Animal(String name) {
     this.name = name;
   }
 
   public void sleep() {
     System.out.println(this.name + " sleeps");
   }
-  
+
   public void eat() {
     System.out.println(this.name + " eats");
   }
-  
+
   public String getName() {
     return this.name;
   }
